9aecd2c2ebc43d335c070bf3cd23576047257d92
gabycosta <80899628+gabycosta@users.noreply.github.com>
2021-04-01 17:28:20 -0300

    Update AC1.ino
    Arrumei o código

--- AC1.ino
+++ AC1.ino
@@ -1,52 +1,67 @@
+//variaveis da led
 const int vermelho = 5;
 const int verde = 6;
 const int azul = 7
 
 bool estadoLedVermelho = false;
 
+//adiçao de um botao
 const int botao1 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
 const int botaoDelay = 100;
 
 void setup()
 {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  
+  //saida led azul
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
+  pinMode(azul, OUTPUT);
   
   Serial.begin(9600);
-	
+	//nome do grupo
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("                           V1.0");
   Serial.println("Grupo: ALCATEIA              ");
 }
-
+//acender e apagar led vermelho
 void loop()
 {
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
   	Serial.println("botao 1 apertado");
-    ledVermelho();
+    ledVermelho(true);
   	lastDebounceTime1 = millis();
   }
-  
-  if(getTemperatura() > 30){
-    ledAzul(true);
-  }else{
-  	ledAzul(falso); 
+  if((millis() - lastDebounceTime2) > botaoDelay && digitalRead(botao2)){
+  	Serial.println("botao 2 apertado");
+    ledVermelho(false);
+  	lastDebounceTime2 = millis();
   }
-  	
+  if(getTemperatura() > 15){
+    ledAzul(true);
+	  Serial.printnl("temperatura elevada");
+  }else{
+  	ledAzul(false); 
+	  Serial.printnl("temperatura ideal");
+  }
+	if(get luminosidade()>5{
+		ledVerde(true);
+		serial.printnl("luminosidade elevada");
+	}else}
+	   ledVerde(false);
+	   Serial.printnl("luminosidade ideal")
+}
   delay(10);
 }
 
-void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
-  digitalWrite(vermelho,estadoLedVermelho);
+void ledVermelho(bool estado){
+  digitalWrite(vermelho,estado);
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+  digitalWrite(verde,estado);
 }
 void ledAzul(bool estado){
 	digitalWrite(azul,estado);
